32ca54af5ba67a2e2ce019012d1f66fc5a2a983c
Jakub Jira <jakub.jira@protonmail.com>
2024-11-22 17:21:04 +0100

    add symlinks to installed applications

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew, ... }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config,... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       nixpkgs.config.allowUnfree = true;
@@ -24,6 +24,7 @@
       environment.systemPackages =
         [ 
 	  pkgs.vim
+	  pkgs.mkalias
 	  pkgs.slack
   	  pkgs.coreutils
         ];
@@ -91,7 +92,26 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+	system.activationScripts.applications.text = let
+        	env = pkgs.buildEnv {
+          		name = "system-applications";
+          		paths = config.environment.systemPackages;
+          		pathsToLink = "/Applications";
+        	};
+      	in
+        	pkgs.lib.mkForce ''
+          		# Set up applications.
+          		echo "setting up /Applications..." >&2
+          		rm -rf /Applications/Nix\ Apps
+          		mkdir -p /Applications/Nix\ Apps
+          		find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          		while read -r src; do
+            		app_name=$(basename "$src")
+            		echo "copying $src" >&2
+            		${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          		done
+        	'';
+    	};
   in
   {
     # Build darwin flake using:
