144d201d684c2f4081700ee91a59cdafb2a190e9
Jakub Jira <jakub.jira@protonmail.com>
2024-11-24 09:35:22 +0100

    add install nix and run darwin to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 softwareupdate --install-rosetta --agree-to-license
 # install nix + run nix-darwin
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+nix run nix-darwin -- switch --flake ~/.config/nix-darwin#tn-macbook
